4bcd79add95c65ea0f49c0e0c9ba7afff4241655
JudithBre <61976163+JudithBre@users.noreply.github.com>
2021-09-14 15:17:53 +0200

    Embedded map via leaflet is Responsive

--- public/uploadfunctionality/einbindenVonLeaflet.js
+++ public/uploadfunctionality/einbindenVonLeaflet.js
@@ -13,27 +13,38 @@ const start_latlng = [lat, lon];
 
 let map = L.map("map", {
   center: start_latlng,
-  zoom: 17,
+  zoom: 17
 });
 
 // Darstellen der Marker auf der Leaflet-Karte und ermoeglichen der Auswahl durch User
 // Wenn die Koordinaten der Marker bekannt sind, dann muessen die Koordinaten nicht erst aus der Datenbank abgefragt werden --> Zeitersparnis!
 let marker1 = L.marker([51.96898469480461, 7.596480628244305]).addTo(map)
-.bindPopup('Marker 1');
-let marker2 = L.marker([51.96903558719034, 7.596355131037076]).addTo(map)
-.bindPopup('Marker 2');
-let marker3 = L.marker([51.96928869301577, 7.596481577258518]).addTo(map)
-.bindPopup('Marker 3');
-let marker4 = L.marker([51.96967930453592, 7.596549508628987]).addTo(map)
+.bindPopup('Marker 1'),
+ marker2 = L.marker([51.96903558719034, 7.596355131037076]).addTo(map)
+.bindPopup('Marker 2'),
+ marker3 = L.marker([51.96928869301577, 7.596481577258518]).addTo(map)
+.bindPopup('Marker 3'),
+ marker4 = L.marker([51.96967930453592, 7.596549508628987]).addTo(map)
 .bindPopup('Marker 4');
 
-// Koordinaten der Marker zwischenspeichern
-let showCoordinates1 = marker1.getLatLng();
-let showCoordinates2 = marker2.getLatLng();
-let showCoordinates3 = marker3.getLatLng();
-let showCoordinates4 = marker4.getLatLng();
+var dreiecksstaender1 = L.layerGroup([marker1]);
+var dreiecksstaender2 = L.layerGroup([marker2]);
+var dreiecksstaender3 = L.layerGroup([marker3]);
+var dreiecksstaender4 = L.layerGroup([marker4]);
 
 let osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
-  attribution: "&copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors",
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors"
 }).addTo(map);
+
+var OpenStreetMap = {
+  "Open Street Map": osm
+};
+var Standorte = {
+    "Standort 1": dreiecksstaender1,
+    "Standort 2": dreiecksstaender2,
+    "Standort 3": dreiecksstaender3,
+    "Standort 4": dreiecksstaender4
+};
+
+L.control.layers(OpenStreetMap, Standorte).addTo(map);
