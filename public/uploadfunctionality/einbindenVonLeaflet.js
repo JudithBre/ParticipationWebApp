ad483eef9f9675fde440c1a4c770f779321cb731
JudithBre <61976163+JudithBre@users.noreply.github.com>
2021-09-14 13:39:40 +0200

    Limit background map to osm

--- public/uploadfunctionality/einbindenVonLeaflet.js
+++ public/uploadfunctionality/einbindenVonLeaflet.js
@@ -37,25 +37,3 @@ let osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors",
 }).addTo(map);
-
-let mapbox = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={access_token}", {
-  maxZoom: 19,
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, " +
-  "<a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, " +
-  "Imagery © <a href=\https://www.mapbox.com/\">Mapbox</a>",
-  id: "mapbox.streets",
-  access_token: token.MAPBOX_TOKEN
-}).addTo(map);
-
-let opentopomap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-  maxZoom: 17,
-  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-});
-
-let baseMaps = {
-  "opentopomap" : opentopomap,
-  "osm": osm,
-  "mapbox": mapbox
-};
-
-L.control.layers(baseMaps).addTo(map);
